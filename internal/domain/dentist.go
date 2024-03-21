66e7ddbfa75149cc3670fc5809401cd7f0f2d47c
Marianaca-29 <mariana.famaf@gmail.com>
2024-03-21 21:51:07 +0100

    struct dentist

--- internal/domain/dentist.go
+++ internal/domain/dentist.go
@@ -1,1 +1,8 @@
 package domain
+// Registrar apellido, nombre y matrícula de los mismos
+type Dentist struct {
+	ID        int    `json:"id"`
+    LastName  string `json:"last_name" binding:"required"`
+    FirstName string `json:"first_name" binding:"required"`
+    License   string `json:"license" binding:"required"`
+}
