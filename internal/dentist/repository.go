4ebc3c4767b12424f4c69028f9833ad60dbac321
Marianaca-29 <mariana.famaf@gmail.com>
2024-03-22 21:28:44 +0100

    repository dentists

--- internal/dentist/repository.go
+++ internal/dentist/repository.go
@@ -1,1 +1,44 @@
 package dentist
+
+import (
+	"DENTAL-CLINIC/internal/domain"
+	"DENTAL-CLINIC/pkg/store"
+	"DENTAL-CLINIC/pkg/web"
+)
+
+type IRepository interface {
+	GetById(id int) (domain.Dentist, error)
+	Create(d domain.Dentist) (domain.Dentist, error)
+}
+
+type Repository struct {
+	Store store.StoreInterface
+}
+
+func NewRepository(Store store.StoreInterface) IRepository {
+	return &Repository{Store}
+}
+
+func (r *Repository) GetById(id int) (domain.Dentist, error) {
+	dent, err := r.Store.GetDentistById(id)
+	if err != nil {
+		return domain.Dentist{}, web.NewNotFoundApiError("/nDentista inexistente")
+	}
+
+	return *dent, nil
+}
+
+func (r *Repository) Create(d domain.Dentist) (domain.Dentist, error) {
+	exists, err := r.Store.GetDentistIdByLicense(d.License)
+	if err != nil {
+		return domain.Dentist{}, err
+	}
+	if exists != 0 {
+		return domain.Dentist{}, web.NewBadRequestApiError("/nEsa matrícula ya existe")
+	}
+	dent, err := r.Store.CreateDentist(d)
+	if err != nil {
+		return domain.Dentist{}, err
+	}
+	return *dent, nil
+}
