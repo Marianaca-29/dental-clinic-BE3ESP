1bc8af3a917b08e17fb3760441918690a9088e7a
paulinaobertib <paulinaobertibusso@gmail.com>
2024-03-22 18:12:32 -0300

    patient methods

--- cmd/server/handler/patient.go
+++ cmd/server/handler/patient.go
@@ -1,1 +1,84 @@
 package handler
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"DENTAL-CLINIC/internal/domain"
+	"DENTAL-CLINIC/internal/patient"
+	"DENTAL-CLINIC/pkg/web"
+
+	"github.com/gin-gonic/gin"
+)
+
+type patientHandler struct {
+	service patient.IService
+}
+
+func NewProductHandler(service patient.IService) *patientHandler {
+	return &patientHandler{
+		service: service,
+	}
+}
+
+func (h *patientHandler) CreatePatient() gin.HandlerFunc {
+	return func(c *gin.Context) { 
+		var patient domain.Patient
+		err := c.ShouldBindJSON(&patient)
+		if (err != nil) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, web.NewBadRequestApiError("Datos del paciente mal ingresados"))
+			return
+		}
+
+		patientResponse, err := h.service.CreatePatient(patient)
+		if (err != nil) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, web.NewBadRequestApiError("No se ha podido crear el paciente"))
+			return
+		}
+		
+		c.JSON(http.StatusOK, gin.H{"paciente" : patientResponse})
+	}
+}
+
+func (h *patientHandler) GetPatientById() gin.HandlerFunc {
+	return func(c *gin.Context) { 
+		idParam := c.Param("id")
+		id, err := strconv.Atoi(idParam)
+		if (err != nil) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, web.NewBadRequestApiError("Id invalido"))
+			return
+		}
+
+		patientResponse, err := h.service.GetPatientById(id)
+		if (err != nil) {
+			var errAPI web.ErrorApi
+			if (err == &errAPI) {
+				c.AbortWithStatusJSON(http.StatusBadRequest, web.NewNotFoundApiError(fmt.Sprintf("No existe el paciente con el id %d", id)))
+				return
+			}
+			c.AbortWithStatusJSON(http.StatusInternalServerError, web.NewInternalServerApiError(err.Error()))
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"paciente" : patientResponse})
+	}
+}
+
+func (h *patientHandler) UpdatePatient() gin.HandlerFunc {
+	return func(c *gin.Context) { 
+
+	}
+}
+
+func (h *patientHandler) UpdatePatientField() gin.HandlerFunc {
+	return func(c *gin.Context) { 
+
+	}
+}
+
+func (h *patientHandler) DeletePatient() gin.HandlerFunc {
+	return func(c *gin.Context) { 
+
+	}
+}
--- internal/patient/service.go
+++ internal/patient/service.go
@@ -1,1 +1,52 @@
 package patient
+
+import (
+	"DENTAL-CLINIC/internal/domain"
+)
+
+type IService interface {
+	CreatePatient (patient domain.Patient) (*domain.Patient, error)
+	GetPatientById (id int) (*domain.Patient, error)
+	UpdatePatient (patient domain.Patient) (*domain.Patient, error)
+	UpdatePatientField (id int, field string, value string) (*domain.Patient, error)
+	DeletePatient (id int) (error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(r Repository) IService {
+	return &service{r}
+}
+
+func (s *service) CreatePatient (patient domain.Patient) (*domain.Patient, error) {
+	patientResponse, err := s.repository.CreatePatient(patient)
+	if (err != nil) {
+		return nil, err
+	}
+
+	return patientResponse, nil
+}
+
+func (s *service) GetPatientById (id int) (*domain.Patient, error) {
+	patientResponse, err := s.repository.GetPatientById(id)
+	if (err != nil) {
+		return nil, err
+	}
+
+	return patientResponse, nil
+}
+
+func (s *service) UpdatePatient (patient domain.Patient) (*domain.Patient, error) {
+	return nil, nil
+}
+
+func (s *service) UpdatePatientField (id int, field string, value string) (*domain.Patient, error) {
+	return nil, nil
+}
+
+func (s *service) DeletePatient (id int) (error) {
+	return nil
+}
+
